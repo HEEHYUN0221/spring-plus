c6098dce9217c68bbd1c09e5d36987a347b67762
heehyun0221 <psr4580@gmail.com>
2025-01-17 11:20:39 +0900

    🔨 refactor : 권한 부분 Spring Security 로 변경

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -11,4 +11,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+    
 }
