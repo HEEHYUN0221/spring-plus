39c59d8ee2f1b9aab5390dc6bb2fb92145e756c5
heehyun0221 <psr4580@gmail.com>
2025-01-25 19:10:07 +0900

    🔨 refactor : UserRole Kotlin으로 변경

--- src/main/java/org/example/expert/domain/user/enums/UserRole.kt
+++ src/main/java/org/example/expert/domain/user/enums/UserRole.kt
@@ -1,24 +1,23 @@
-package org.example.expert.domain.user.enums;
+package org.example.expert.domain.user.enums
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.exception.InvalidRequestException;
-
-import java.util.Arrays;
+import lombok.Getter
+import lombok.RequiredArgsConstructor
+import org.example.expert.domain.common.exception.InvalidRequestException
+import java.util.*
 
 @Getter
 @RequiredArgsConstructor
-public enum UserRole {
+enum class UserRole(role: String, description: String) {
     ADMIN("ROLE_ADMIN", "관리자 권한"),
     USER("ROLE_USER", "사용자 권한");
 
-    private final String role;
-    private final String description;
-
-    public static UserRole of(String role) {
-        return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(role))
+    companion object {
+        @JvmStatic
+        fun of(role: String?): UserRole {
+            return Arrays.stream(entries.toTypedArray())
+                .filter { r: UserRole -> r.name.equals(role, ignoreCase = true) }
                 .findFirst()
-                .orElseThrow(() -> new InvalidRequestException("유효하지 않은 UerRole"));
+                .orElseThrow { InvalidRequestException("유효하지 않은 UerRole") }
+        }
     }
 }
